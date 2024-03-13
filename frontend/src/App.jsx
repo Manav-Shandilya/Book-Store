36a3805f87d54cb8c4ab6a2d835a4b1aaef7f9f0
Manav Shandilya <manavshandilya2004@gmail.com>
2024-03-13 18:43:18 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import CreateBook from './pages/CreateBooks';
 import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
-
+import { FaBook } from "react-icons/fa";
 
 const App = () => {
   return (
+    <>
+    <h1 class="text-4xl text-black font-bold text-center p-5 bg-blue-300 shadow-xl rounded-l mb-10">Book Store</h1>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBook />} />
@@ -16,6 +18,7 @@ const App = () => {
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
+    </>
   );
 };
 
